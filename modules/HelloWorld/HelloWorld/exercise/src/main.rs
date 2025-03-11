d7201a055e6ef2bbea4e936a01881bdbb578a457
Benedict Jackson <bavjackson@gmail.com>
2025-03-11 19:59:31 +0000

    Initial commit

---
+++ modules/HelloWorld/HelloWorld/exercise/src/main.rs
@@ -0,0 +1,8 @@
+mod adder;
+
+fn main() {
+    // println!("{}", adder::adder::add(1, 2));
+    adder::adder::add(1, 3);
+    println!("Hello World!");
+}
+
---
+++ modules/HelloWorld/HelloWorld/exercise/tests/test_hello_world.rs
@@ -0,0 +1,8 @@
+#[test]
+fn it_prints_hello_world() {
+    use std::process::Command;
+
+    let output = Command::new("cargo").arg("run").output().unwrap().stdout;
+
+    assert_eq!(output, b"Hello World!\n".to_vec());
+}
