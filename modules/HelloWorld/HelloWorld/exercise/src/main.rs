462f94e83efb44a8a48c2b77b0dffc5565221d23
Benedict Jackson <bavjackson@gmail.com>
2025-03-12 13:23:28 +0000

    tidy up template

--- modules/HelloWorld/HelloWorld/exercise/src/main.rs
+++ modules/HelloWorld/HelloWorld/exercise/src/main.rs
@@ -1,8 +1,6 @@
 mod adder;
 
 fn main() {
-    // println!("{}", adder::adder::add(1, 2));
-    adder::adder::add(1, 3);
     println!("Hello World!");
 }
 
--- modules/HelloWorld/HelloWorld/exercise/tests/test_hello_world.rs
+++ modules/HelloWorld/HelloWorld/exercise/tests/test_hello_world.rs
@@ -1,8 +1,11 @@
-#[test]
-fn it_prints_hello_world() {
-    use std::process::Command;
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_prints_hello_world() {
+        use std::process::Command;
 
-    let output = Command::new("cargo").arg("run").output().unwrap().stdout;
+        let output = Command::new("cargo").arg("run").output().unwrap().stdout;
 
-    assert_eq!(output, b"Hello World!\n".to_vec());
+        assert_eq!(output, b"Hello World!\n".to_vec());
+    }
 }
